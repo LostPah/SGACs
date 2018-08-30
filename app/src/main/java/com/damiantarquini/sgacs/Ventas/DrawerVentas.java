e047ea272ad19c31503aa21587fa16a7f34f4ad1
Tutu <lost.pah@gmail.com>
2018-08-30 15:46:20 -0300

    Actualizado

--- app/src/main/java/com/damiantarquini/sgacs/Ventas/DrawerVentas.java
+++ app/src/main/java/com/damiantarquini/sgacs/Ventas/DrawerVentas.java
@@ -1,4 +1,4 @@
-package com.damiantarquini.sgacs;
+package com.damiantarquini.sgacs.Ventas;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.damiantarquini.sgacs.R;
 
 public class DrawerVentas extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
